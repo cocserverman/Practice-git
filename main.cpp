7d83119a5b8e0619f6e3f539ff4a65067f9f431d
cocserverman <91588916+cocserverman@users.noreply.github.com>
2025-04-17 14:13:18 +0330

    Second Commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
+void print (int Stunum){
+
+    cout <<"Your number = "<< Stunum <<endl;
+}
+
 int main(){
 
+    int numb;
     cout << "Hello World";
-
+    numb=4036013005;
+    print(numb);
     return 0;
 }
